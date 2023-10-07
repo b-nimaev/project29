c2eebc4fc0da800877a55930ef204e22de7c3710
unknown <alexandrbnimaev@yandex.ru>
2023-10-07 09:12:57 +0800

    update

---
+++ bot1/src/database.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+// @ts-ignore
+const username = encodeURIComponent("alexandr")
+const password = encodeURIComponent(<string>process.env.password?.replace(/"/g, ''))
+
+console.log(username)
+console.log(password)
+
+mongoose.connect(`mongodb://${username}:${password}@87.236.22.124:27017/burlang?authSource=admin`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+} as any).catch(error => { console.error(error) });
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB!');
+});
